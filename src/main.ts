9a3576b951cad2ce42c9da4605813238d5620e15
Elizaveta Razumenko <elizaveta.razumenko@pargoo.com>
2024-08-06 23:46:57 +0300

    change dockerfile

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import helmet from 'helmet';
 
 config();
 
+console.log('Environment Variables:');
+console.log(`PORT: ${process.env.PORT}`);
+console.log(`PG_PORT: ${process.env.PG_PORT}`);
+console.log(`PG_HOST: ${process.env.PG_HOST}`);
+console.log(`PG_DB: ${process.env.PG_DB}`);
+console.log(`PG_USER: ${process.env.PG_USER}`);
+console.log(`PG_PASS: ${process.env.PG_PASS}`);
+
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
